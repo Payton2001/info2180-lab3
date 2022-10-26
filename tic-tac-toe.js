067e68ec32324e8d29b74650c5e1f7c787bc7956
NiaPayton <shadeanpayton@gmail.com>
2022-10-26 12:01:53 -0500

    Added winner function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,9 @@
-"use strict";
+"use strict"
 
 window.onload = function(){
 
-    const X  = 'X';
-    const O = 'O';
+    const X  = 'X'
+    const O = 'O'
     const winOrder = [
         [0, 1, 2],
         [3, 4, 5],
@@ -13,41 +13,85 @@ window.onload = function(){
         [2, 5, 8],
         [0, 4, 8],
         [2, 4, 6],
-    ];
-    let whoseTurn = X;
-    let status = ['','','','','','','','',''];
-    let gameRunning = false;
-    const btn = document.getElementsByClassName('#btn');
-    let square = document.querySelectorAll('#board div');
-    let count = square.length;
+    ]
+    let whoseTurn = X
+    let state = ['','','','','','','','','']
+    let gameRunning = true
+    const btn = document.getElementsByClassName('#btn')
+    const square = document.querySelectorAll('#board div')
+    const status = document.getElementById('status')
+    let count = square.length
 
+    btn.addEventListener('click', gameBtn)
 
     for (let i = 0; i < count; i++){
-        square[i].classList.add('square');
+
+        square[i].classList.add('square')
         square[i].addEventListener('click', function(){
+
         
-            gameRunning = true;
-            if(whoseTurn == 'X'){
-                this.innerHTML = X;
-                this.classList.add(X);
-                whoseTurn = O;
+            if(whoseTurn == 'X' && gameRunning){
+                this.innerHTML = X
+                this.classList.add(X)
+                state[i] = whoseTurn
+                whoseTurn = O
+                console.log(state)
+                if(isWinner(X) && gameRunning){
+                    status.textContent = 'Congratulations! X is the winner!'
+                    status.classList.add('you-won')
+                    gameRunning == false
+                }
+                isDraw()
             
             }
-            else if (whoseTurn == 'O'){
-                this.innerHTML = O;
-                this.classList.add(O);
-                whoseTurn = X;
+            else if (whoseTurn == 'O' && gameRunning){
+                this.innerHTML = O
+                this.classList.add(O)
+                state[i] = whoseTurn
+                whoseTurn = X
+                console.log(state)
+                if(isWinner(O) && gameRunning){
+                    status.textContent = 'Congratulations! O is the winner!'
+                    status.classList.add('you-won')
+                    gameRunning == false
+                }
+                isDraw()
                 
             }
-        });
+            if (gameRunning == false){return}
+
+        })
 
         //Set Hover
         square[i].addEventListener('mouseover', function(){
             this.classList.add('hover')
-        });
+        })
         square[i].addEventListener('mouseout', function(){
             this.classList.remove('hover')
         })
+
     }
+
+
+
+    function isWinner(whoseTurn){
+        return winOrder.some(arr =>{
+            return arr.every(index =>{
+                return square[index].classList.contains(whoseTurn)
+            })
+        })
+    }
+
+    function isDraw(){
+        if((!isWinner(X) || !isWinner(O)) && !(state.includes(''))){
+            status.textContent = 'There was no winner the game was a draw!'
+            status.classList.add('status')
+            gameRunning = false
+
+        }
+    }
+
+    
+
         
 }
